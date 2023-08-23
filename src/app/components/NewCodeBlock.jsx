7b5774681777c9be4bfe2cc4543a1d27e590ba40
ErnestTramp <ernesttramp@gmail.com>
2023-08-23 19:58:05 +0100

    cant figure it out

--- src/app/components/NewCodeBlock.jsx
+++ src/app/components/NewCodeBlock.jsx
@@ -1,27 +1,44 @@
-'use client'
+"use client";
+import SaveSnippet from "./SaveCodeBlock";
 import hljs from "highlight.js";
 import React, { useEffect, useState } from "react";
 
 export default function NewCodeBlock() {
-    const [detectedLanguage, setDetectedLanguage] = useState(null);
-    useEffect(() => {
-        hljs.initHighlightingOnLoad();
-      }, []);
+  var [detectedLanguage, setDetectedLanguage] = useState(null);
+  var [codeValue, setCodeValue] = useState(""); // State to manage code value
 
-      const handleCodeChange = (event) => {
-        const code = event.target.value;
-        const language = hljs.highlightAuto(code).language;
-        setDetectedLanguage(language);
-      };
+  useEffect(() => {
+    hljs.highlightAll();
+  }, []);
 
-      return (
-        <>
-        <p className="language">{detectedLanguage}</p>
+  const handleCodeChange = (event) => {
+    var code = event.target.value;
+    var language = hljs.highlightAuto(code).language;
+    setDetectedLanguage(language);
+  };
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    saveSnippet(detectedLanguage, codeValue);
+  };
+
+  const placeholders = "language";
+
+  return (
+    <>
+      <p className="language">
+        {detectedLanguage === undefined ? placeholders : detectedLanguage}
+      </p>
+      <form onSubmit={handleFormSubmit}>
         <textarea
-        className="new-code"
-            onChange={handleCodeChange}
-            placeholder="Enter some code..."
-          ></textarea>
-        </>
-      )
+          className="new-code"
+          onChange={handleCodeChange}
+          placeholder="Enter some code..."
+        ></textarea>
+        <button className="form-button" type="submit">
+          Send
+        </button>
+      </form>
+    </>
+  );
 }
--- src/app/new/page.js
+++ src/app/new/page.js
@@ -1,14 +1,14 @@
 import Header from "../components/Header";
 import NewCodeBlock from "../components/NewCodeBlock";
 
-export default async function Page() {
+export default function Page() {
   return (
     <>
       <Header page="Home" nav="/" />
       <main>
         <form>
           <NewCodeBlock />
-          <button className="form-button">Send :D</button>
+          <button className="form-button">Send</button>
         </form>
       </main>
     </>
